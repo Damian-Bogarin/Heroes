4c964a56c42248a19d5431d3c681d8f006315ca4
Damian-Bogarin <boga_nicdam@outlook.com>
2021-10-17 22:46:25 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,37 @@
+let express = require ('express')
+
+let app = express()
+
+let path = require('path')
+
+app.get("/home",(req,res) => {
+res.sendFile(path.join(__dirname, '/DH-Heroes/views/index.html'))
+})
+
+app.get("/babbage",(req,res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/babbage.html'))
+})
+app.get("/berners-lee",(req,res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/berners-lee.html'))
+})
+app.get("/clarke",(req,res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/clarke.html'))
+})
+app.get("/hamilton",(req,res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/hamilton.html'))
+})
+app.get("/hopper",(req,res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/hopper.html'))
+})
+app.get("/lovelace",(req,res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/lovelace.html'))
+})
+app.get("/turing",(req,res) => {
+    res.sendFile(path.join(__dirname, '/DH-Heroes/views/turing.html'))
+})
+
+// app.use(express.static('public'))
+
+
+
+app.listen(3030, () => console.log("Servidor Levantado"))
